6ef243064f02407a87752db83b5f94064235b330
Chistyakov Alexey <chskela@gmail.com>
2022-09-11 19:50:19 +0300

    Add Contact info Properties to Owner. Closes #27

--- src/main/kotlin/com/chskela/petclinic/model/Owner.kt
+++ src/main/kotlin/com/chskela/petclinic/model/Owner.kt
@@ -4,6 +4,9 @@ data class Owner(
     override var id: Long = -1L,
     override val firstName: String,
     override val lastName: String,
+    val address: String,
+    val city: String,
+    val telephone: String,
     val pets: Set<Pet> = mutableSetOf()
 ) :
     Person(id, firstName, lastName) {
